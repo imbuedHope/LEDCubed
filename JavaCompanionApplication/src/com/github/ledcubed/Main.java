ffdbd62268be35588fece3b47351c8231ed0345c
imbuedHope <imbuedHope@hotmail.com>
2014-06-11 11:49:45 -0500

    Added GUI Base

--- JavaCompanionApplication/src/com/github/ledcubed/Main.java
+++ JavaCompanionApplication/src/com/github/ledcubed/Main.java
@@ -1,7 +1,39 @@
 package com.github.ledcubed;
 
+import javax.swing.JFrame;
+
+import com.github.ledcubed.gui.GUIFrame;
+
+/**
+ * Launch class
+ * @author imbuedHope
+ * 
+ */
 public class Main {
+	
+	static int CubeSideX = 8;
+	static int CubeSideY = 8;
+	static int CubeSideZ = 8;
+
+	static int FrameGenerationRate = 500;
+	
 	public static void main(String[] args) {
+		//TODO: Load values from `.conf` file in place of hardcoded values
 		
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame();
+
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setLocationByPlatform(true);
+				frame.setTitle("LED Cube Manager");
+
+				frame.add(new GUIFrame(CubeSideX, CubeSideY, CubeSideZ, FrameGenerationRate));
+
+				frame.setResizable(false);
+				frame.setVisible(true);
+				frame.pack();
+			}
+		});
 	}
 }
--- JavaCompanionApplication/src/com/github/ledcubed/gui/GUIFrame.java
+++ JavaCompanionApplication/src/com/github/ledcubed/gui/GUIFrame.java
@@ -1,8 +1,16 @@
 package com.github.ledcubed.gui;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class GUIFrame extends JPanel {
+	static final int XLEN = 800;
+	static final int YLEN = 450;
+	
+	public GUIFrame(int cubeSideX, int cubeSideY, int cubeSideZ, int frameGenerationRate) {
+		this.setPreferredSize(new Dimension(XLEN, YLEN));
+	}
 
 	private static final long serialVersionUID = -2167912679009145686L;
 
